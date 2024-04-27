d2ce384fb85a3c099ef13a7450d8e0177239dfd2
João Macieira <jp.macieira@uol.com.br>
2024-04-27 18:47:12 -0300

    feat: add shadcn ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,3 @@
-import { AppBar } from './components/AppBar';
-import { TodosList } from './components/TodosList';
-import { useRenderCounter } from './hooks/useRenderCounter';
-
-import './store/createStore';
-
 export function App() {
-  useRenderCounter('App');
-
-  return (
-    <>
-      <AppBar />
-      <TodosList />
-    </>
-  );
+  return <h1>Hello World</h1>;
 }
